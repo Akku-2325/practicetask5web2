4515fde2e955d032fe2d38e4e2d290323660deba
Akku-2325 <231696@astanait.edu.kz>
2025-02-02 19:43:27 +0500

    Initial commit

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
-app.get('/'
-, (req, res) => {
-res.send('Hello, World! Deployed with Heroku');
+
+app.get('/', (req, res) => {
+  res.send('Hello, World! Deployed with Heroku');
 });
+
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
-console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
