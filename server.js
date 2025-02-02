d647fd076d0219507875231ae013235e23e4e43d
Akku-2325 <231696@astanait.edu.kz>
2025-02-02 20:03:16 +0500

    Added dynamic responses and new routes

--- server.js
+++ server.js
@@ -1,12 +1,33 @@
 const express = require('express');
 const app = express();
-app.get('/'
-, (req, res) => {
-res.send('Hello, World! Deployed with Heroku');
+
+app.get('/', (req, res) => {
+  let name = req.query.name; 
+  if (name) {
+    res.send(`Hello, ${name}! Deployed with Heroku. This is a new feature.`);
+  } else {
+     res.send('Hello, World! Deployed with Heroku. This is a new feature.');
+  }
 });
+
+app.get('/items/:itemId', (req, res) => {
+  const itemId = req.params.itemId; 
+  res.send(`Viewing item with ID: ${itemId}`);
+});
+
+app.get('/sum', (req, res) => {
+    const a = parseInt(req.query.a);
+    const b = parseInt(req.query.b);
+
+    if (isNaN(a) || isNaN(b)) {
+        res.status(400).send('Invalid parameters. Please provide numbers for a and b.');
+        return;
+    }
+     res.send(`The sum of ${a} and ${b} is: ${a+b}`);
+})
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
